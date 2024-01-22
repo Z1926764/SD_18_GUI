1d4bd0d757614515c88d15450e450083dbe0c180
z1926764 <z1926764@students.niu.edu>
2024-01-22 13:43:18 -0600

    first commit

--- app/celery.py
+++ app/celery.py
@@ -1,12 +1,14 @@
 # django_celery/celery.py
 
-
 import os
 
 from celery import Celery
+from celery import chain
 from celery.schedules import crontab
 from datetime import timedelta
 from app.tasks import get_pressure
+from app.tasks import read_pressure_switch
+from app.tasks import control_pressure
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
@@ -15,21 +17,40 @@ app = Celery("app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+'''
+@app.task
+def chained_pressure_tasks(arg1, arg2):
+    chained_task = chain(
+        app.tasks.get_pressure.s(arg1, arg2),
+        app.tasks.read_pressure_switch.s(arg1, arg2),
+    )
+'''
 
 app.conf.beat_schedule = {
     'measure-pressure': {
         'task': 'app.tasks.get_pressure',
-        'schedule': timedelta(seconds=.25),
+        'schedule': timedelta(milliseconds=100),
         'args': (16, 16),
     },
+
+    '''
     'pressure-switch-state': {
         'task': 'app.tasks.read_pressure_switch',
-        'schedule': timedelta(seconds=0),
+        'schedule': timedelta(milliseconds=10000),
         'args': (1,1),
     },
+    '''
+    
+    '''
+    'chained-pressure-tasks': {
+        'task': 'app.tasks.chained_pressure_tasks',
+        'schedule': timedelta(milliseconds=2500),
+        'args': (16,16),
+    },
+    '''
     'update-control-pressure': {
         'task': 'app.tasks.control_pressure',
-        'schedule': timedelta(seconds=.25),
+        'schedule': timedelta(milliseconds=2500),
         'args': (1,1),
     }
 }
--- app/tasks.py
+++ app/tasks.py
@@ -13,37 +13,45 @@ from collections import deque
 #   within the project folder
 # View the beat schedule of this task within app/celery.py. Right now,
 #   it is set to run every 5 seconds upon execution of the Celery worker
+i2c = busio.I2C(board.SCL, board.SDA)
+
+adc = ADC.ADS1115(i2c)
+
+adc.gain = 2/3
+
+supP = AnalogRead(adc, ADC.P0)
+conP = AnalogRead(adc, ADC.P1)
+
+'''
+@shared_task()
+def structure(self, *args):
+    pass
+'''
+
 @shared_task()
 def get_pressure(self, *args):
     relation = 1 # This defines the voltage to pressure linear relationship
     # import the PressurePoint model and update it every time this task runs
     from .models import PressurePoint
     # Following Example from library 
-    # https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15
-
-    i2c = busio.I2C(board.SCL, board.SDA)
-
-    adc = ADC.ads1115
-
-    supP = AnalogRead(adc, ADC.P0)
-    conP = AnalogRead(adc, ADC.P1)
-
-
+    # https://github.com/adafruit/Adafruit_CircuitPython_ADS1x1
     currentPressure = conP.voltage * relation
     print("CURRENT PRESSURE: " + str(currentPressure))
 
     # get the current date and time
     now = datetime.datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-
+    
     # create a new entry within the PressurePoint table
     entry = PressurePoint(time=date_time, pressure=currentPressure)
     entry.save() 
-
+    var2 = read_pressure_switch(self)
+    print(f'Switch State: {var2}')
     return currentPressure
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+@shared_task
 def read_pressure_switch(self, *args):
     switchState = GPIO.input(4)
     return switchState
@@ -54,16 +62,19 @@ GPIO.setup(7,GPIO.OUT)
 servoValvePwm = GPIO.PWM(7, 1000) # pin num, frequency
 servoValvePwm.start(pwmVal) # duty cycle  
 # not sure if have to use args pointer and unpack
+@shared_task()
 def control_pressure(self, *args):
     from .models import GlobalValues
-    setpoint = GlobalValues.models.get(setPoint)
-    currentPressure = GlobalValues.models.get(currentPressure)
+    variables = GlobalValues.objects.first()
+    setpoint = variables.setPoint
+    currentPressure = variables.currentPressure
     kp = 1 
     kd = 1
     ki = 1
     global er
     global pwm
-    er.append((setpoint-currentPressure))
-    gain = (kp*er(4)) + (kd*(er(4)-er(3))/2) + (ki*sum(er))
-    servoValvePwm.ChangeDutyCycle(max(min(int(pwmval + gain),100),0))
+    val = float(setpoint - currentPressure)
+    er.append(val)
+    gain = (kp*er[4]) + (kd*(er[4]-er[3])/2) + (ki*sum(er))
+    servoValvePwm.ChangeDutyCycle(max(min(int(pwmVal + gain),100),0))
     return 0
